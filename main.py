3d20cb1b8f317bc99c78270977c5d5ebc1b22f3c
IGxRohit <creative07vibez@gmail.com>
2024-03-29 15:05:27 +0530

    h

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template      
+
+app=Flask(__name__)
+
+@app.route("/aboutus")
+def homepage():
+    return render_template("aboutus.html")
+@app.route("/")
+def indexpage():
+    return render_template("home.html")
+
+
+if __name__=="__main__":
+    app.run(port=1000,debug=True)
